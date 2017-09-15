9d93a674e81f80028664f60666e7fd4583874b6b
Simon Hempenstall <simonjhemp@hotmail.com>
2017-09-15 17:49:38 +0100

    Prevent blank required fields and duplicate email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates_presence_of :username, :email
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates_uniqueness_of :email
 end
--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -4,4 +4,26 @@ RSpec.feature "Sign up", type: :feature do
   scenario "A new user can sign up" do
     expect { sign_up }.to change(User, :count).by(1)
   end
-end 
+
+  describe "Blank fields" do
+    scenario "Username field cannot be blank" do
+      expect { sign_up(username: nil) }.to_not change(User, :count)
+    end
+
+    scenario "Email field cannot be blank" do
+      expect { sign_up(email: nil) }.to_not change(User, :count)
+    end
+
+    scenario "Email must be correctly formatted" do
+      expect { sign_up(email: "not a valid email") }.to_not change(User, :count)
+    end
+  end
+
+  describe "Duplicate details" do
+    scenario "Email address must be unique" do
+      @user = User.create(username: 'Tom', email: 'Tom@fakegmail.com',
+      address: '40 The Street', tel_no: '123456789')
+      expect { sign_up }.to_not change(User, :count)
+    end
+  end
+end
